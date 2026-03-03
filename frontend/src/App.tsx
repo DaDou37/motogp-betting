434370fe3aed8f91fd75c3011aa8e1ea75e96b4f
DaDou37 <riera.david94@gmail.com>
2026-03-03 16:19:20 +0100

    test de route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import Accueil from './pages/Accueil';
-import Auth from './pages/Auth';
 import Pilotes from './pages/Pilotes';
 import GrandsPrix from './pages/GrandsPrix';
 import Parier from './pages/Parier';
 import Classement from './pages/Classement';
 import Profil from './pages/Profil';
 import Admin from './pages/Admin';
+import AuthPage from './pages/Auth';
 
 
 
@@ -18,7 +18,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Accueil />} />
-          <Route path="/auth" element={<Auth />} />
+          <Route path="/auth" element={<AuthPage />} />
           <Route path="/pilotes" element={<Pilotes />} />
           <Route path="/grandsprix" element={<GrandsPrix />} />
           <Route path="/parier" element={<Parier />} />
